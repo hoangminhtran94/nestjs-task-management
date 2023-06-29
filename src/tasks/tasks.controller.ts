bbcaaf826cc9ea03aa462e3ac90a2312d5db4a21
minhhoangtran94 <95241885+hoangminhtran94@users.noreply.github.com>
2023-06-29 02:21:04 -0400

    adds Task model and define get service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from "@nestjs/common";
+import { TasksService } from "./tasks.service";
+import { Task } from "./tasks.model";
 
-@Controller('tasks')
-export class TasksController {}
+@Controller("tasks")
+export class TasksController {
+  constructor(private readonly taskService: TasksService) {
+    this.taskService = taskService;
+  }
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.taskService.getAllTasks();
+  }
+}
